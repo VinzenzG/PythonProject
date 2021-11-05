0ec4eeedff392eb141c6fd94167d4cb3c2a72637
Vinzenz Gempe <vinzenz.gempe@gmail.com>
2021-11-05 15:18:33 +0100

    basic

--- agents/common.py
+++ agents/common.py
@@ -6,7 +6,7 @@ BoardPiece = np.int8  # The data type (dtype) of the board
 NO_PLAYER = BoardPiece(0)  # board[i, j] == NO_PLAYER where the position is empty
 PLAYER1 = BoardPiece(1)  # board[i, j] == PLAYER1 where player 1 (player to move first) has a piece
 PLAYER2 = BoardPiece(2)  # board[i, j] == PLAYER2 where player 2 (player to move second) has a piece
-
+PLAYERtest = BoardPiece(5)
 BoardPiecePrint = str  # dtype for string representation of BoardPiece
 NO_PLAYER_PRINT = BoardPiecePrint(' ')
 PLAYER1_PRINT = BoardPiecePrint('X')
@@ -20,12 +20,27 @@ class GameState(Enum):
     IS_DRAW = -1
     STILL_PLAYING = 0
 
-
 def initialize_game_state() -> np.ndarray:
     """
     Returns an ndarray, shape (6, 7) and data type (dtype) BoardPiece, initialized to 0 (NO_PLAYER).
     """
+    row, column = 6, 7
+    field = np.ndarray((row, column), BoardPiece())
+    field.fill(NO_PLAYER)
+    return field
+
     raise NotImplementedError
+
+
+def add_first_line():
+    return "|==============|"
+
+
+def add_last_line():
+    return "\n|==============|\n|0 1 2 3 4 5 6 |"
+
+def test_game_string() -> str:
+    return "|==============|\n|              |\n|              |\n|    X X       |\n|    O X X     |\n|  O X O O     |\n|  O O X X     |\n|==============|\n|0 1 2 3 4 5 6 |"
 
 
 def pretty_print_board(board: np.ndarray) -> str:
@@ -44,6 +59,29 @@ def pretty_print_board(board: np.ndarray) -> str:
     |==============|
     |0 1 2 3 4 5 6 |
     """
+    result = ""
+    my_board = []
+
+    for row in board:
+        my_board.append(row)
+    my_board = my_board[:: -1]
+
+    result += add_first_line()
+    #
+    for row in my_board:
+        result += "\n"
+        result += "|"
+        for x in row:
+            if str(x) == "0":
+                result += "  "
+            if str(x) == "1":
+                result += PLAYER1_PRINT + " "
+            if str(x) == "2":
+                result += PLAYER2_PRINT + " "
+        result += "|"
+    result += add_last_line()
+
+    return result
     raise NotImplementedError()
 
 
@@ -53,6 +91,32 @@ def string_to_board(pp_board: str) -> np.ndarray:
     This is quite useful for debugging, when the agent crashed and you have the last
     board state as a string.
     """
+    row, column = 6, 7
+    field = np.ndarray((row, column), BoardPiece())
+    field.fill(PLAYERtest)
+    lines = pp_board.split('\n')
+    lines.pop(0)
+    lines.pop(7)
+    lines.pop(6)
+    newline = []
+    x = 0
+
+    for line in lines:
+        line = line[1: -1]
+        line = line[0: 15: 2]
+        y = 0
+        for piece in line:
+
+            if piece == " ":
+                field[x][y] = NO_PLAYER
+            if piece == "X":
+                field[x][y] = PLAYER1
+            if piece == "O":
+                field[x][y] = PLAYER2
+            y += 1
+        x += 1
+    field = field[:: -1]
+    return field
     raise NotImplementedError()
 
 
@@ -63,6 +127,15 @@ def apply_player_action(
     Sets board[i, action] = player, where i is the lowest open row. The modified
     board is returned. If copy is True, makes a copy of the board before modifying it.
     """
+    if copy:
+        cop = board
+
+    for x in range(5):
+        if board[x][action] == NO_PLAYER:
+            board[x][action] = player
+            break
+
+    return board
     raise NotImplementedError()
 
 
@@ -72,6 +145,7 @@ def connected_four(
     """
     Returns True if there are four adjacent pieces equal to `player` arranged
     in either a horizontal, vertical, or diagonal line. Returns False otherwise.
+
     If desired, the last action taken (i.e. last column played) can be provided
     for potential speed optimisation.
     """
--- tests/test_common.py
+++ tests/test_common.py
@@ -1,5 +1,9 @@
 import numpy as np
-from agents.common import BoardPiece, NO_PLAYER
+from numpy import int8
+
+from agents.common import BoardPiece, NO_PLAYER, PLAYER1, PLAYER2, pretty_print_board, initialize_game_state, \
+    test_game_string, apply_player_action, string_to_board
+
 
 def test_initialize_game_state():
     from agents.common import initialize_game_state
@@ -10,3 +14,52 @@ def test_initialize_game_state():
     assert ret.dtype == BoardPiece
     assert ret.shape == (6, 7)
     assert np.all(ret == NO_PLAYER)
+
+
+def test_pretty_print_board():
+    from agents.common import pretty_print_board, initialize_game_state
+    board = initialize_game_state()
+    board[0][0] = PLAYER1
+    ret = pretty_print_board(initialize_game_state())
+
+    ret1 = np.ndarray((6, 7), BoardPiece())
+    ret1.fill(PLAYER1)
+    ret2 = np.ndarray((6, 7), BoardPiece())
+    ret2.fill(PLAYER2)
+
+    assert isinstance(ret, str)
+    assert ret.partition('\n')[0] == "|==============|"
+    assert ret.count('\n') == 8
+    assert ret.split('\n')[7] == "|==============|"
+    assert ret.split('\n')[8] == "|0 1 2 3 4 5 6 |"
+    assert ret.split('\n')[1] == "|              |"
+    assert pretty_print_board(ret1).split('\n')[1] == "|X X X X X X X |"
+    assert pretty_print_board(ret2).split('\n')[1] == "|O O O O O O O |"
+    assert pretty_print_board(board).split('\n')[6] == "|X             |"
+
+
+def test_string_to_board():
+    from agents.common import string_to_board
+    ret = pretty_print_board(initialize_game_state())
+    retTest = test_game_string()
+    board = string_to_board(retTest)
+    assert isinstance(string_to_board(retTest), np.ndarray)
+    assert board.shape == (6, 7)
+    assert board[0][1] == PLAYER2
+
+
+def test_apply_player_action():
+    board = initialize_game_state()
+    boardhigh = string_to_board(test_game_string())
+    action = 0
+    player = PLAYER1
+    copy = False
+    ret = apply_player_action(board, action, player, copy)
+    assert isinstance(ret, np.ndarray)
+    assert apply_player_action(board, action, player, copy)[0][0] == 1
+    assert apply_player_action(boardhigh, 1, player, copy)[2][1] == 1
+
+def test_connected_four():
+    board = string_to_board(test_game_string())
+
+
